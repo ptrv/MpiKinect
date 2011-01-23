0f325e40883f3112b24123439a723f0d2250395f
simbur <simibur@googlemail.com>
2011-01-23 22:42:17 +0100

    buttons added to colorChooser

--- src/mpikinect/ColorChooserScreen.java
+++ src/mpikinect/ColorChooserScreen.java
@@ -10,24 +10,31 @@ import processing.core.PImage;
 
 public class ColorChooserScreen extends Screen {
 
-	Button graffitiRed, graffitiGreen, graffitiBlue;
+	Button graffitiRed, graffitiGreen, graffitiOrange, graffitiBlue, undo;
 	
 	public ColorChooserScreen(AppMain p) {
 		super(p);
-		this.background = pApplet.loadImage("testBackgroundColor.png");
+		this.background = pApplet.loadImage("screen3_bg_1024.png");
+		this.background.resize(AppMain.frameWidth, AppMain.frameHeight);
 		
-		PImage imgButton1e = pApplet.loadImage("buttons/button_graffiti_red_1.png");
-		PImage imgButton1f = pApplet.loadImage("buttons/button_graffiti_red_2.png");
-		PImage imgButton2e = pApplet.loadImage("buttons/button_graffiti_red_1.png");
-		PImage imgButton2f = pApplet.loadImage("buttons/button_graffiti_red_2.png");
-		PImage imgButton3e = pApplet.loadImage("buttons/button_graffiti_red_1.png");
-		PImage imgButton3f = pApplet.loadImage("buttons/button_graffiti_red_2.png");
-		adjustImageSize(imgButton1e, imgButton1f, imgButton2e, imgButton2f, imgButton3e, imgButton3f);
+		PImage imgButton1e = pApplet.loadImage("buttons/button_3_1_empty.png");
+		PImage imgButton1f = pApplet.loadImage("buttons/button_3_1_full.png");
+		PImage imgButton2e = pApplet.loadImage("buttons/button_3_2_empty.png");
+		PImage imgButton2f = pApplet.loadImage("buttons/button_3_2_full.png");
+		PImage imgButton3e = pApplet.loadImage("buttons/button_3_3_empty.png");
+		PImage imgButton3f = pApplet.loadImage("buttons/button_3_3_full.png");
+		PImage imgButton4e = pApplet.loadImage("buttons/button_3_4_empty.png");
+		PImage imgButton4f = pApplet.loadImage("buttons/button_3_4_full.png");
+		PImage imgButton5e = pApplet.loadImage("buttons/button_3_5_empty.png");
+		PImage imgButton5f = pApplet.loadImage("buttons/button_3_5_full.png");
+		adjustImageSize(imgButton1e, imgButton1f, imgButton2e, imgButton2f, imgButton3e, imgButton3f, imgButton4e, imgButton4f, imgButton5e, imgButton5f);
 
-		this.graffitiRed = new Button(imgButton1e, imgButton1f, (int)(0.05*AppMain.frameWidth), (int)(0.80*AppMain.frameHeight), Button.LOADING_BOTTOM_TO_TOP, p);
-		this.graffitiGreen = new Button(imgButton2e, imgButton2f, (int)(0.20*AppMain.frameWidth), (int)(0.80*AppMain.frameHeight), Button.LOADING_BOTTOM_TO_TOP, p);
-		this.graffitiBlue = new Button(imgButton3e, imgButton3f, (int)(0.35*AppMain.frameWidth), (int)(0.80*AppMain.frameHeight), Button.LOADING_BOTTOM_TO_TOP, p);
-
+		this.graffitiRed = 		new Button(imgButton1e, imgButton1f, (int)(0.05*AppMain.frameWidth), (int)(0.77*AppMain.frameHeight), Button.LOADING_BOTTOM_TO_TOP, p);
+		this.graffitiGreen = 	new Button(imgButton2e, imgButton2f, (int)(0.20*AppMain.frameWidth), (int)(0.77*AppMain.frameHeight), Button.LOADING_BOTTOM_TO_TOP, p);
+		this.graffitiOrange = 	new Button(imgButton3e, imgButton3f, (int)(0.35*AppMain.frameWidth), (int)(0.77*AppMain.frameHeight), Button.LOADING_BOTTOM_TO_TOP, p);
+		this.graffitiBlue = 	new Button(imgButton4e, imgButton4f, (int)(0.50*AppMain.frameWidth), (int)(0.77*AppMain.frameHeight), Button.LOADING_BOTTOM_TO_TOP, p);
+		this.undo = 			new Button(imgButton5e, imgButton5f, (int)(0.80*AppMain.frameWidth), (int)(0.77*AppMain.frameHeight), Button.LOADING_RIGHT_TO_LEFT, p);
+		
 	}
 
 	@Override
@@ -35,9 +42,8 @@ public class ColorChooserScreen extends Screen {
 		pApplet.image(background, 0,0);
 		
         if(graffitiRed.isPointOnButton(p)) {
-            if(graffitiRed.hover(pApplet.millis())) { //ok, button is hovered, update overlay animation
+            if(graffitiRed.hover(pApplet.millis())) { 
                 System.out.println("graffitiRed is clicked!!!");
-                pApplet.setExpertMode(true);
                 pApplet.setDrawColor(Color.RED);
                 pApplet.setCurrentScreen(Screens.DRAWING);
                 graffitiRed.release();
@@ -46,12 +52,12 @@ public class ColorChooserScreen extends Screen {
         else
         	graffitiRed.release();
 
-		graffitiRed.draw();
+        graffitiRed.draw();
 
-        if(graffitiGreen.isPointOnButton(p)) {
-            if(graffitiGreen.hover(pApplet.millis())) { //ok, button is hovered, update overlay animation
+		
+		if(graffitiGreen.isPointOnButton(p)) {
+            if(graffitiGreen.hover(pApplet.millis())) { 
                 System.out.println("graffitiGreen is clicked!!!");
-                pApplet.setExpertMode(true);
                 pApplet.setDrawColor(Color.GREEN);
                 pApplet.setCurrentScreen(Screens.DRAWING);
                 graffitiGreen.release();
@@ -62,10 +68,22 @@ public class ColorChooserScreen extends Screen {
 
 		graffitiGreen.draw();
 		
+        if(graffitiOrange.isPointOnButton(p)) {
+            if(graffitiOrange.hover(pApplet.millis())) { 
+                System.out.println("graffitiOrange is clicked!!!");
+                pApplet.setDrawColor(Color.ORANGE);
+                pApplet.setCurrentScreen(Screens.DRAWING);
+                graffitiOrange.release();
+            }
+        }
+        else
+        	graffitiOrange.release();
+
+        graffitiOrange.draw();
+		
         if(graffitiBlue.isPointOnButton(p)) {
-            if(graffitiBlue.hover(pApplet.millis())) { //ok, button is hovered, update overlay animation
+            if(graffitiBlue.hover(pApplet.millis())) { 
                 System.out.println("graffitiBlue is clicked!!!");
-                pApplet.setExpertMode(true);
                 pApplet.setDrawColor(Color.BLUE);
                 pApplet.setCurrentScreen(Screens.DRAWING);
                 graffitiBlue.release();
@@ -75,6 +93,18 @@ public class ColorChooserScreen extends Screen {
         	graffitiBlue.release();
 
 		graffitiBlue.draw();
+		
+		
+        if(undo.isPointOnButton(p)) {
+            if(undo.hover(pApplet.millis())) { 
+                System.out.println("TODO: implement undo action!");
+            }
+        }
+        else
+        	undo.release();
+
+        undo.draw();
+
 
 	}
 
